61fd0ee4fce71a55570a84455ab3376337a478e0
syedahmedbinwaseem <57592567+syedahmedbinwaseem@users.noreply.github.com>
2020-11-23 09:09:59 +0500

    Minor

--- lib/Vendor/vendorProduct.dart
+++ lib/Vendor/vendorProduct.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 
@@ -358,17 +359,30 @@ class _VendorProductState extends State<VendorProduct>
                                         builder: (context) {
                                           return AlertDialog(
                                             title: Text(
-                                              'Description',
-                                              style: TextStyle(
-                                                  fontFamily: 'Segoe',
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                            content: Text(
-                                              menSnap.docs[index]
-                                                  ['description'],
-                                              style: TextStyle(
-                                                fontFamily: 'Segoe',
-                                              ),
+                                                menSnap.docs[index]['name'],
+                                                style: TextStyle(
+                                                    fontFamily: 'Segoe',
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                            content: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              mainAxisSize: MainAxisSize.min,
+                                              children: [
+                                                Text('Description:',
+                                                    style: TextStyle(
+                                                        fontFamily: 'Segoe',
+                                                        fontWeight:
+                                                            FontWeight.w700)),
+                                                SizedBox(height: 10),
+                                                Text(
+                                                  menSnap.docs[index]
+                                                      ['description'],
+                                                  style: TextStyle(
+                                                    fontFamily: 'Segoe',
+                                                  ),
+                                                ),
+                                              ],
                                             ),
                                           );
                                         });
@@ -1417,16 +1431,21 @@ class _VendorProductState extends State<VendorProduct>
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           children: [
-                                            Text(
-                                                menSnap.docs[index]['name'] ==
-                                                        null
-                                                    ? ''
-                                                    : menSnap.docs[index]
-                                                        ['name'],
-                                                style: TextStyle(
-                                                    fontFamily: 'Segoe',
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 20)),
+                                            Container(
+                                              width: 80,
+                                              height: 27,
+                                              child: Text(
+                                                  menSnap.docs[index]['name'] ==
+                                                          null
+                                                      ? ''
+                                                      : menSnap.docs[index]
+                                                          ['name'],
+                                                  style: TextStyle(
+                                                      fontFamily: 'Segoe',
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 20)),
+                                            ),
                                             SizedBox(
                                               height: 10,
                                             ),
@@ -1776,17 +1795,32 @@ class _VendorProductState extends State<VendorProduct>
                                         builder: (context) {
                                           return AlertDialog(
                                             title: Text(
-                                              'Description',
-                                              style: TextStyle(
-                                                  fontFamily: 'Segoe',
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                            content: Text(
-                                              womenSnap.docs[index]
-                                                  ['description'],
-                                              style: TextStyle(
-                                                fontFamily: 'Segoe',
-                                              ),
+                                                womenSnap.docs[index]['name'],
+                                                style: TextStyle(
+                                                    fontFamily: 'Segoe',
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                            content: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  'Description',
+                                                  style: TextStyle(
+                                                      fontFamily: 'Segoe',
+                                                      fontWeight:
+                                                          FontWeight.w700),
+                                                ),
+                                                SizedBox(height: 10),
+                                                Text(
+                                                  womenSnap.docs[index]
+                                                      ['description'],
+                                                  style: TextStyle(
+                                                    fontFamily: 'Segoe',
+                                                  ),
+                                                ),
+                                              ],
                                             ),
                                           );
                                         });
@@ -2820,16 +2854,22 @@ class _VendorProductState extends State<VendorProduct>
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           children: [
-                                            Text(
-                                                womenSnap.docs[index]['name'] ==
-                                                        null
-                                                    ? ''
-                                                    : womenSnap.docs[index]
-                                                        ['name'],
-                                                style: TextStyle(
-                                                    fontFamily: 'Segoe',
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 20)),
+                                            Container(
+                                              width: 80,
+                                              height: 27,
+                                              child: Text(
+                                                  womenSnap.docs[index]
+                                                              ['name'] ==
+                                                          null
+                                                      ? ''
+                                                      : womenSnap.docs[index]
+                                                          ['name'],
+                                                  style: TextStyle(
+                                                      fontFamily: 'Segoe',
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 20)),
+                                            ),
                                             SizedBox(
                                               height: 10,
                                             ),
@@ -3180,17 +3220,34 @@ class _VendorProductState extends State<VendorProduct>
                                         builder: (context) {
                                           return AlertDialog(
                                             title: Text(
-                                              'Description',
-                                              style: TextStyle(
-                                                  fontFamily: 'Segoe',
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                            content: Text(
-                                              kidsSnap.docs[index]
-                                                  ['description'],
-                                              style: TextStyle(
-                                                fontFamily: 'Segoe',
-                                              ),
+                                                kidsSnap.docs[index]['name'],
+                                                style: TextStyle(
+                                                    fontFamily: 'Segoe',
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                            content: Column(
+                                              mainAxisSize: MainAxisSize.min,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  'Description',
+                                                  style: TextStyle(
+                                                      fontFamily: 'Segoe',
+                                                      fontWeight:
+                                                          FontWeight.w700),
+                                                ),
+                                                SizedBox(
+                                                  height: 10,
+                                                ),
+                                                Text(
+                                                  kidsSnap.docs[index]
+                                                      ['description'],
+                                                  style: TextStyle(
+                                                    fontFamily: 'Segoe',
+                                                  ),
+                                                ),
+                                              ],
                                             ),
                                           );
                                         });
@@ -4224,16 +4281,22 @@ class _VendorProductState extends State<VendorProduct>
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           children: [
-                                            Text(
-                                                kidsSnap.docs[index]['name'] ==
-                                                        null
-                                                    ? ''
-                                                    : kidsSnap.docs[index]
-                                                        ['name'],
-                                                style: TextStyle(
-                                                    fontFamily: 'Segoe',
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 20)),
+                                            Container(
+                                              width: 80,
+                                              height: 27,
+                                              child: Text(
+                                                  kidsSnap.docs[index]
+                                                              ['name'] ==
+                                                          null
+                                                      ? ''
+                                                      : kidsSnap.docs[index]
+                                                          ['name'],
+                                                  style: TextStyle(
+                                                      fontFamily: 'Segoe',
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 20)),
+                                            ),
                                             SizedBox(
                                               height: 10,
                                             ),
@@ -5569,4 +5632,139 @@ Widget editDismiss() {
       alignment: Alignment.centerLeft,
     ),
   );
+}
+
+class ScrollingText extends StatefulWidget {
+  final String text;
+  final TextStyle textStyle;
+  final Axis scrollAxis;
+  final double ratioOfBlankToScreen;
+
+  ScrollingText({
+    @required this.text,
+    this.textStyle,
+    this.scrollAxis: Axis.horizontal,
+    this.ratioOfBlankToScreen: 0.25,
+  }) : assert(
+          text != null,
+        );
+
+  @override
+  State<StatefulWidget> createState() {
+    return ScrollingTextState();
+  }
+}
+
+class ScrollingTextState extends State<ScrollingText>
+    with SingleTickerProviderStateMixin {
+  ScrollController scrollController;
+  double screenWidth;
+  double screenHeight;
+  double position = 0.0;
+  Timer timer;
+  final double _moveDistance = 3.0;
+  final int _timerRest = 100;
+  GlobalKey _key = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    WidgetsBinding.instance.addPostFrameCallback((callback) {
+      startTimer();
+    });
+  }
+
+  void startTimer() {
+    if (_key.currentContext != null) {
+      double widgetWidth =
+          _key.currentContext.findRenderObject().paintBounds.size.width;
+      double widgetHeight =
+          _key.currentContext.findRenderObject().paintBounds.size.height;
+
+      timer = Timer.periodic(Duration(milliseconds: _timerRest), (timer) {
+        double maxScrollExtent = scrollController.position.maxScrollExtent;
+        double pixels = scrollController.position.pixels;
+        if (pixels + _moveDistance >= maxScrollExtent) {
+          if (widget.scrollAxis == Axis.horizontal) {
+            position = (maxScrollExtent -
+                        screenWidth * widget.ratioOfBlankToScreen +
+                        widgetWidth) /
+                    2 -
+                widgetWidth +
+                pixels -
+                maxScrollExtent;
+          } else {
+            position = (maxScrollExtent -
+                        screenHeight * widget.ratioOfBlankToScreen +
+                        widgetHeight) /
+                    2 -
+                widgetHeight +
+                pixels -
+                maxScrollExtent;
+          }
+          scrollController.jumpTo(position);
+        }
+        position += _moveDistance;
+        scrollController.animateTo(position,
+            duration: Duration(milliseconds: _timerRest), curve: Curves.linear);
+      });
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+  }
+
+  Widget getBothEndsChild() {
+    if (widget.scrollAxis == Axis.vertical) {
+      String newString = widget.text.split("").join("\n");
+      return Center(
+        child: Text(
+          newString,
+          style: widget.textStyle,
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+    return Center(
+        child: Text(
+      widget.text,
+      style: widget.textStyle,
+    ));
+  }
+
+  Widget getCenterChild() {
+    if (widget.scrollAxis == Axis.horizontal) {
+      return Container(width: screenWidth * widget.ratioOfBlankToScreen);
+    } else {
+      return Container(height: screenHeight * widget.ratioOfBlankToScreen);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (timer != null) {
+      timer.cancel();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      key: _key,
+      scrollDirection: widget.scrollAxis,
+      controller: scrollController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        getBothEndsChild(),
+        getCenterChild(),
+        getBothEndsChild(),
+      ],
+    );
+  }
 }
